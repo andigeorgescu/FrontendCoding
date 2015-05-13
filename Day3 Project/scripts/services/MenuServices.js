9e7f4913d18b12749f6fd41d729231d95ca1352a
Andi Georgescu <andi94.g@gmail.com>
2015-05-13 18:21:07 +0300

    Day3 Project

---
+++ Day3 Project/scripts/services/MenuServices.js
@@ -0,0 +1,11 @@
+hrApp.value('employeeActionsService', [
+        // TODO #11 - add menu items
+        {   label: "Employee list",
+            url: "#/employeeslist"
+        },
+        {   label: "Add employee",
+            url: "#/employeeadd"
+        }
+
+]
+);
